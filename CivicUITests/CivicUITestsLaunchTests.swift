0485549272e19a1b7e79fae602f805b4074e0b5b
moriarty83 <moriarty83@gmail.com>
2022-01-12 15:34:26 -0500

    Initial Commit

---
+++ CivicUITests/CivicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CivicUITestsLaunchTests.swift
+//  CivicUITests
+//
+//  Created by Christopher M Moriarty on 1/12/22.
+//
+
+import XCTest
+
+class CivicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
